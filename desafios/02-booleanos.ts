6866d29ce4b0768557e5be019d019c0cd6ede6d0
Robson Barbosa Souza <powerrob92@gmail.com>
2024-07-13 09:59:44 +0000

    chore: my solution of boolean names

--- desafios/02-booleanos.ts
+++ desafios/02-booleanos.ts
@@ -10,22 +10,22 @@ const necessaryHeight = 130
 
 const currentHour = new Date().getHours()
 
-const parkOpen = currentHour > 9 && currentHour < 18
+const isParkOpen = currentHour > 9 && currentHour < 18
 
-if (!parkOpen) {
+if (!isParkOpen) {
   throw new Error('O parque está fechado!')
 }
 
-const ticket = user.hasTicket
+const hasTicket = user.hasTicket
 
-if (!ticket) {
+if (!hasTicket) {
   throw new Error('O Diego não possui um bilhete para entrar no parque!')
 }
 
-const enterToy = user.height > necessaryHeight
+const hasHeightToEnterToy = user.height > necessaryHeight
 
-if (!enterToy) {
+if (!hasHeightToEnterToy) {
   throw new Error('O Diego não pode entrar no brinquedo!')
-} 
+}
 
 console.log('O Diego se divertiu muito! :)')
